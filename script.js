4e552fbfe5842549f962c855b59cad19db2c1012
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-04-05 14:14:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,35 @@ var button = document.getElementById("submit");
 
 button.addEventListener("click", function(event)
 {
-  if()
-  text.value = "";
-  language.value = "";
-  console.log("Wiped Input");
+  console.log("Button Clicked");
+  var textText = text.value.toLowerCase();
+  var languageText = language.value.toLowerCase();
+  if(text.checkValidity() && language.checkValidity())
+  {
+    switch(textText)
+    {
+      case 'hello':
+        console.log("Hello");
+        switch(languageText)
+        {
+          case 'german':
+            output.innerHTML = "Guten Tag";
+            console.log("German");
+            break;
+        }
+        break;
+      case 'goodbye':
+        console.log("Goodbye");
+        break;
+      case 'yes':
+        console.log("Yes");
+        break;
+      case 'no':
+        console.log("No");
+        break;
+      case 'maybe':
+        console.log("Maybe");
+        break;
+    }
+  }
 });
