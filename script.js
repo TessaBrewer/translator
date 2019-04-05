7917182e71a58ad2a2524a59b2cbcaa17896f010
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-04-05 13:57:39 -0400

    Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+var text = document.getElementById('text');
+var language = document.getElementById("language");
+var output = document.getElementById("output");
+var button = document.getElementById("submit");
+
+button.addEventListener("click", function(event)
+{
+  if()
+  text.value = "";
+  language.value = "";
+  console.log("Wiped Input");
+});
